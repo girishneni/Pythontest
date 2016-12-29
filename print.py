5cd797407f43eeddd456c60f013996fb2514a031
Girish kumar Bhaskar <girishkumarbhaskar@Girishs-MacBook-Pro.local>
2016-12-29 16:07:04 -0500

    basic printing statemenst in python

---
+++ print.py
@@ -0,0 +1,72 @@
+
+# coding: utf-8
+
+# In[2]:
+
+print ("Welcome to the world of python")
+
+
+# In[3]:
+
+#printing with an append
+print(" Welcome to the", "world of pyhton")
+
+
+# In[4]:
+
+print('hello this is jupyter', ' an associate package of python')
+
+
+# In[5]:
+
+#printing a single quote ' using an escape character 
+print('This is it!\'')
+
+
+# In[14]:
+
+#printing using a C style command
+print('The float number %5.3f' % (100.333333))
+
+
+# In[15]:
+
+#printing using variable
+var1=200
+s='this is right'
+print( 'variable=%d string=%s' % (var1, s))
+
+
+# In[18]:
+
+#printing multiple times
+print("hello!"*6)
+
+
+# In[21]:
+
+#printing in a new line
+print("hello!!"+'\n' +"This is girish")
+
+
+# In[25]:
+
+#printing using a .format operator
+print('hello {} to the world of {}'.format('welcome','python'))
+
+
+# In[30]:
+
+#similarly
+print('hello {0} to world of {1}'.format('welcome','python'))
+
+
+# In[31]:
+
+print('hello {s1} to world of {s2}'.format(s1='welcome',s2='python'))
+
+
+# In[ ]:
+
+
+
