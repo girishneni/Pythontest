be49a31af6707e327cccdc1be2cfd8717fc344f5
Girish kumar Bhaskar <girishkumarbhaskar@Girishs-MacBook-Pro.local>
2017-01-05 19:43:36 -0500

    counting the charracters in string

--- countchar.py
+++ countchar.py
@@ -1,10 +1,10 @@
 my_str="hello world"
-words=my_str.split()
-print(len(words))
+word=my_str.split()
+print(len(word))
 countChar=0
-for i in range(len(words)):
+for i in range(len(word)):
     for k in range(int(len(my_str)/2)):
-        print(words[i][k])
+        print(word[i][k])
         countChar+=1
 
 print(countChar)
